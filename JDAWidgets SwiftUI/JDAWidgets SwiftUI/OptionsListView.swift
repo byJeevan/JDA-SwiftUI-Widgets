480bca7d84e60605a7d0c6e12532c48dbd00bf65
Jeevan <jeevan8082@gmail.com>
2024-08-24 23:07:01 +0530

    Simplified OptionsListView

--- JDAWidgets SwiftUI/JDAWidgets SwiftUI/OptionsListView.swift
+++ JDAWidgets SwiftUI/JDAWidgets SwiftUI/OptionsListView.swift
@@ -8,71 +8,84 @@
 
 import SwiftUI
 
+
+let options: [WidgetOptions] = [
+  
+  WidgetOptions(id: 1,
+                shortName: "Dropdown",
+                description: "Simple dropdown shown and when selected value will be filled.",
+                view: AnyView(Dropdown())),
+  
+  WidgetOptions(id: 2,
+                shortName: "Country Picker",
+                description: "Dropdown will be shown along with country flags (offline). This is packed as bundle and works for offline usage.",
+                view: AnyView(CountryPicker())),
+  
+  WidgetOptions(id: 3,
+                shortName: "Floating Button",
+                description: "Floating Button which will reveal more buttons when tapped.",
+                view: AnyView(FloatingButton())),
+  
+  WidgetOptions(id: 4,
+                shortName: "Expandable (Accordion) List",
+                description: "List that can expand or collapse.",
+                view: AnyView(ExpandableList())),
+  
+  WidgetOptions(id: 5,
+                shortName: "Basic Activity Indicator",
+                description: "Simplest activity indicator for SwiftUI.",
+                view: AnyView(BasicActivityIndicator(animationSwitch: true))),
+  
+  WidgetOptions(id: 6,
+                shortName: "Endless List (Lazy loading)",
+                description: "List that scrolls infinitely and fetches new content whenever a user reaches the end of the list.",
+                view: AnyView(EndlessList())),
+  
+  WidgetOptions(id: 7,
+                shortName: "Custom Dialog Example",
+                description: "Bake a custom dialog view and toggle visibility with animation from the center.",
+                view: AnyView(CustomDialogExampleView())),
+  
+  WidgetOptions(id: 8,
+                shortName: "Star Rating",
+                description: "Star rating widget for all needs.",
+                view: AnyView(StarRatingDemoView())),
+  
+  WidgetOptions(id: 9,
+                shortName: "Tags",
+                description: "Group of Tags takes action and alignment can be controlled to Left/Center/Right.",
+                view: AnyView(TagsDemoView())),
+  
+  WidgetOptions(id: 10,
+                shortName: "Bottom To Top sheet",
+                description: "Simple bottom to top sheet appears with animation.",
+                view: AnyView(BottomToTopSheetView()))
+]
+
+
+// MARK: - Options Model
 struct WidgetOptions: Identifiable {
   var id: Int
   let shortName: String
   let description: String
+  let view: AnyView
 }
 
+// MARK: - Widget Option List view
 struct OptionsListView: View {
-  
-  var options: [WidgetOptions] = [
-    WidgetOptions(id: 1, shortName: "Dropdown", description: "Simple dropdown shown when tap event."),
-    WidgetOptions(id: 2, shortName: "Country Picker", description: "Dropdown will be shown along with country flags (offline). This is packed as bundle and works for offline usage."),
-    WidgetOptions(id: 3, shortName: "Floating Button", description: "Floating Button which will reveal with more buttons when tapped."),
-    WidgetOptions(id: 4, shortName: "Expandable (Accordion) List", description: "List that can expand or collapse."),
-    WidgetOptions(id: 5, shortName: "Basic Activity Indicator", description: "Simplest activity indicator for SwiftUI."),
-    WidgetOptions(id: 6, shortName: "Endless List (Lazy loading)", description: "List that scrolls infinitely and fetches new content whenever a user reaches the end of the list"),
-    WidgetOptions(id: 7, shortName: "Custom Dialog Example", description: "Bake a custom dialog view and toggle visibility with animation from center."),
-    WidgetOptions(id: 8, shortName: "Star Rating", description: "Star rating widget for all needs."),
-    WidgetOptions(id: 9, shortName: "Tags", description: "Group of Tags takes action and alignment can be controlled to Left/Center/Right"),
-    WidgetOptions(id: 10, shortName: "Bottom To Top sheet", description: "Simple bottom to top sheet appears with animation.")
-  ]
-  
-  @ViewBuilder func WidgetDetail(wOption: WidgetOptions) -> some View {
-    switch wOption.id {
-      case 1:
-        Dropdown()
-      case 2:
-        CountryPicker() //FIXIT: Height of dropdown
-      case 3:
-        FloatingButton()
-      case 4:
-        ExpandableList()
-      case 5:
-        BasicActivityIndicator(animationSwitch: true)
-      case 6:
-        EndlessList()
-      case 7:
-        CustomDialogExampleView()
-      case 8:
-        StarRatingDemoView()
-      case 9:
-        TagsDemoView()
-        
-      case 10:
-        BottomToTopSheetView()
-        
-      default:
-        Text("Widget Not implemented")
-    }
-  }
-  
   var body: some View {
-    
     NavigationView {
       List(options) { option in
-        NavigationLink {
-          WidgetDetail(wOption: option)
-        } label: {
+        NavigationLink(destination: option.view) {
           WidgetRow(wOption: option)
         }
-      }.navigationBarHidden(true)
+      }
+      .navigationBarHidden(true)
     }
-    
   }
 }
 
+// MARK: - Widget Option Row
 struct WidgetRow: View {
   var wOption: WidgetOptions
   
@@ -84,6 +97,7 @@ struct WidgetRow: View {
   }
 }
 
+// MARK: - Preview
 struct OptionsListView_Previews: PreviewProvider {
   static var previews: some View {
     OptionsListView()
